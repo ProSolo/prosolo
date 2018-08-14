fc6029baac25e836858c4b98df61980b72b6cc7b
David Laehnemann <david.laehnemann@hhu.de>
2018-08-14 16:54:45 +0200

    flamegraphing: correct plugin config attribute syntax

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
-#![cfg_attr(feature="flame_it", feature(plugin, custom_attribute))]
-#![cfg_attr(feature="flame_it", plugin(flamer))]
-
-#![cfg_attr(feature="flame_it_details", feature(plugin, custom_attribute))]
-#![cfg_attr(feature="flame_it_details", plugin(flamer))]
+#![cfg_attr(any(feature="flame_it", feature="flame_it_details"), feature(plugin, custom_attribute))]
+#![cfg_attr(any(feature="flame_it", feature="flame_it_details"), plugin(flamer))]
 
 #[cfg(any(feature="flame_it", feature="flame_it_details"))]
 extern crate flame;
