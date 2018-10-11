0c8a687eb18cf49973c764b48439a60d9c63184e
David Laehnemann <david.laehnemann@hhu.de>
2018-10-11 16:32:47 +0200

    retire rust internal flamegraphing in favor of perf flamegraphing

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,3 @@
-#![cfg_attr(any(feature="flame_it", feature="flame_it_details"), feature(plugin, custom_attribute))]
-#![cfg_attr(any(feature="flame_it", feature="flame_it_details"), plugin(flamer))]
-
-#[cfg(any(feature="flame_it", feature="flame_it_details"))]
-extern crate flame;
-
 #[macro_use]
 extern crate log;
 extern crate fern;
@@ -19,7 +13,6 @@ extern crate libprosic;
 extern crate rust_htslib;
 extern crate bio;
 
-#[cfg(any(feature="flame_it", feature="flame_it_details"))]
 use std::fs::File;
 use std::process;
 use std::mem;
@@ -28,66 +21,6 @@ use clap::App;
 
 pub mod call;
 pub mod estimate;
-
-/// Implementation as given by @ebarnard at: https://github.com/TyOverby/flame/issues/33#issuecomment-352312506
-#[cfg(any(feature="flame_it", feature="flame_it_details"))]
-fn write_flamegraph() {
-    let mut spans = flame::threads().into_iter().next().unwrap().spans;
-    merge_spans(&mut spans);
-    flame::dump_html_custom(&mut File::create("flame-graph.html").unwrap(), &spans)
-        .unwrap();
-}
-
-#[cfg(any(feature="flame_it", feature="flame_it_details"))]
-fn merge_spans(spans: &mut Vec<flame::Span>) {
-    if spans.is_empty() {
-        return;
-    }
-
-    // Sort so spans to be merged are adjacent and spans with the most children are
-    // merged into to minimise allocations.
-    spans.sort_unstable_by(|s1, s2| {
-        let a = (&s1.name, s1.depth, usize::max_value() - s1.children.len());
-        let b = (&s2.name, s2.depth, usize::max_value() - s2.children.len());
-        a.cmp(&b)
-    });
-
-    // Copy children and sum delta from spans to be merged
-    let mut merge_targets = vec![0];
-    {
-        let mut spans_iter = spans.iter_mut().enumerate();
-        let (_, mut current) = spans_iter.next().unwrap();
-        for (i, span) in spans_iter {
-            if current.name == span.name && current.depth == span.depth {
-                current.delta += span.delta;
-                let mut children = mem::replace(&mut span.children, Vec::new());
-                current.children.extend(children.into_iter());
-            } else {
-                current = span;
-                merge_targets.push(i);
-            }
-        }
-    }
-
-    // Move merged spans to the front of the spans vector
-    for (target_i, &current_i) in merge_targets.iter().enumerate() {
-        spans.swap(target_i, current_i);
-    }
-
-    // Remove duplicate spans
-    spans.truncate(merge_targets.len());
-
-    // Merge children of the newly collapsed spans
-    for span in spans {
-        merge_spans(&mut span.children);
-    }
-}
-
-
-#[cfg(not(any(feature="flame_it", feature="flame_it_details")))]
-fn write_flamegraph() {
-}
-
 
 fn main() {
     // parse command line
@@ -137,5 +70,4 @@ fn main() {
         }
     }
 
-    write_flamegraph();
 }
